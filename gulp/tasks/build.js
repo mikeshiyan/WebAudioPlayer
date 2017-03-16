122cccf51cf04f9199ff9de50b74a8bf79741a4a
Mike Shiyan <dev@mike.pp.ua>
2017-03-16 15:21:44 +0200

    Added minified script

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -2,6 +2,8 @@
 
 var gulp = require('gulp');
 var replace = require('gulp-replace');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 var pump = require('pump');
 
 gulp.task('build', function (cb) {
@@ -9,7 +11,14 @@ gulp.task('build', function (cb) {
 
   pump([
     gulp.src('src/WebAudioPlayer.js'),
+
+    // Create dist.
     replace('{{ version }}', pkg.version),
+    gulp.dest('dist'),
+
+    // Create minified version.
+    uglify(),
+    rename('WebAudioPlayer.min.js'),
     gulp.dest('dist')
   ], cb);
 });
